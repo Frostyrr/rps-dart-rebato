a97fe83fdd65b5396d05b272c5954975a30aead4
Frostyrr <marcelson.rebato@gmail.com>
2026-09-25 12:09:24 +0800

    refactor main: add do while and final result, add clearScreen function

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -20,19 +20,26 @@ List<String> getPlayerName() {
   return [playerOne, playerTwo];
 }
 
+void clearScreen() {
+  for (int i = 0; i < 30; i++) {
+    print("");
+  }
+  print("(screen cleared)");
+}
+
 List<String?> getMove(String? playerOne, String? playerTwo) {
   String? playerOneMove;
   String? playerTwoMove;
 
-  while (validateMove(playerOneMove) == null) {
+  do {
     stdout.write("$playerOne, enter your move (rock/paper/scissors): ");
     playerOneMove = stdin.readLineSync()?.toLowerCase();
-  }
+  } while (validateMove(playerOneMove) == null);
 
-  while (validateMove(playerTwoMove) == null) {
+  do {
     stdout.write("$playerTwo, enter your move (rock/paper/scissors): ");
     playerTwoMove = stdin.readLineSync()?.toLowerCase();
-  }
+  } while (validateMove(playerTwoMove) == null);
 
   return [playerOneMove, playerTwoMove];
 }
@@ -54,7 +61,6 @@ String? decideWinner(playerOne, playerTwo, playerOneMove, playerTwoMove) {
   String? result;
 
   if (playerOneMove == playerTwoMove) {
-    result = "Draw";
     return null;
   }
 
@@ -81,26 +87,54 @@ void main() {
   List<String> playerNames = getPlayerName();
   String playerOne = playerNames[0];
   String playerTwo = playerNames[1];
-  List<String?> playerMoves = getMove(playerOne, playerTwo);
-  String? playerOneMove = playerMoves[0];
-  String? playerTwoMove = playerMoves[1];
+  String? choice;
+  int rounds = 1;
+  do {
+    print("===== ROCK, PAPER, SCISSORS =====");
+    print("--- Rounds $rounds ---");
+    List<String?> playerMoves = getMove(playerOne, playerTwo);
+    String? playerOneMove = playerMoves[0];
+    String? playerTwoMove = playerMoves[1];
 
-  String? result = decideWinner(
-    playerOne,
-    playerTwo,
-    playerOneMove,
-    playerTwoMove,
-  );
+    String? result = decideWinner(
+      playerOne,
+      playerTwo,
+      playerOneMove,
+      playerTwoMove,
+    );
 
-  if (result != null) {
-    print("Result: $result");
+    if (result != null) {
+      print("Result: $result");
 
-    if (result.contains(playerOne)) {
-      playerOneScore++;
-    } else if (result.contains(playerTwo)) {
-      playerTwoScore++;
+      if (result.contains(playerOne)) {
+        playerOneScore++;
+      } else if (result.contains(playerTwo)) {
+        playerTwoScore++;
+      }
+    } else {
+      print("Result: Draw");
     }
+    print("Score -> $playerOne: $playerOneScore | $playerTwo: $playerTwoScore");
+
+    stdout.write("Play again? (y/n): ");
+    choice = stdin.readLineSync()?.trim().toLowerCase();
+
+    if (choice == "y") {
+      rounds++;
+    }
+  } while (choice != "n");
+
+  String overAllWinner;
+
+  if (playerOneScore > playerTwoScore) {
+    overAllWinner = "$playerOne";
+  } else if (playerOneScore < playerTwoScore) {
+    overAllWinner = "$playerTwo";
+  } else {
+    overAllWinner = "Draw";
   }
 
-  print("Score -> $playerOne: $playerOneScore | $playerTwo: $playerTwoScore");
+  print("===== FINAL SCORE =====");
+  print("$playerOne: $playerOneScore | $playerTwo: $playerTwoScore");
+  print("Overall winner: $overAllWinner");
 }
